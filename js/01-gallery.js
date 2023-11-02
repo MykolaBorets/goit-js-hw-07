ce540f3b1b13a9b55635ad577c5fa9bebf1cfead
MykolaBorets <mykola.borets@gmail.com>
2023-11-02 23:21:09 +0100

    changed as in comments

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,46 +2,29 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
-
-function createGalleryItem(item) {
-  const galleryItem = document.createElement("li");
-  galleryItem.classList.add("gallery__item");
-
-  const link = document.createElement("a");
-  link.classList.add("gallery__link");
-  link.href = item.original;
-
-  const image = document.createElement("img");
-  image.classList.add("gallery__image");
-  image.src = item.preview;
-  image.alt = item.description;
-  image.setAttribute("data-source", item.original);
-
-  link.addEventListener("click", function (event) {
-    event.preventDefault();
-  });
-
-  link.appendChild(image);
-  galleryItem.appendChild(link);
-
-  return galleryItem;
-}
-
-function appendGalleryItems(items) {
-  const fragment = [];
-
-  items.forEach((item) => {
-    const galleryItem = createGalleryItem(item);
-    fragment.push(galleryItem);
-  });
-
-  gallery.append(...fragment);
-}
-
-appendGalleryItems(galleryItems);
-
+createGalleryItem(galleryItems);
 gallery.addEventListener("click", onGalleryClick);
 let instance = null;
+
+function createGalleryItem(items) {
+  const galleryItem = items
+    .map(
+      ({ preview, original, description }) => `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+  gallery.insertAdjacentHTML("beforeend", galleryItem);
+  return galleryItem;
+}
 
 function onGalleryClick(event) {
   event.preventDefault();
@@ -52,11 +35,11 @@ function onGalleryClick(event) {
       <img src='${largeImageURL}' width="1400" height="900">
     `);
     instance.show();
+    window.addEventListener("keydown", (event) => {
+      if (event.code === "Escape" && instance) {
+        instance.close();
+        window.removeEventListener("keydown", (event));
+      }
+    });
   }
 }
-
-window.addEventListener("keydown", (event) => {
-  if (event.code === "Escape" && instance) {
-    instance.close();
-  }
-});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,42 +2,27 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
+createGalleryItem(galleryItems);
 
-function createGalleryItem(item) {
-  const galleryItem = document.createElement("li");
-  galleryItem.classList.add("gallery__item");
-
-  const link = document.createElement("a");
-  link.classList.add("gallery__link");
-  link.href = item.original;
-
-  const image = document.createElement("img");
-  image.classList.add("gallery__image");
-  image.src = item.preview;
-  image.alt = item.description;
-
-  link.addEventListener("click", function (event) {
-    event.preventDefault();
-  });
-
-  link.appendChild(image);
-  galleryItem.appendChild(link);
-
+function createGalleryItem(items) {
+  const galleryItem = items
+    .map(
+      ({ preview, original, description }) => `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+  gallery.insertAdjacentHTML("beforeend", galleryItem);
   return galleryItem;
 }
-
-function appendGalleryItems(items) {
-  const fragment = [];
-
-  items.forEach((item) => {
-    const galleryItem = createGalleryItem(item);
-    fragment.push(galleryItem);
-  });
-
-  gallery.append(...fragment);
-}
-
-appendGalleryItems(galleryItems);
 
 let lightbox = new SimpleLightbox(".gallery a", {
   captionsData: 'alt',
